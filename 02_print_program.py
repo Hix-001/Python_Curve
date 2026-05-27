ce1caafd0d315c81a9569494ba6f98736920edd6
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-05-27 20:50:08 +0530

    learnt : variables and datatypes

---
+++ 02_print_program.py
@@ -0,0 +1,39 @@
+print ("Harsh is my name.")
+print ("My age is 20 years.")
+
+print ("Harsh is my name." , "My age is 20 years.")
+
+print(23)
+print(23.5)
+print(35)
+print(35+25)
+print("The sum of 35 and 25 is", 35+25)
+
+name = "Harsh" 'HARSH JHA'
+age = 20
+print (name)
+print (age) 
+
+age = 20
+age2 = age
+print (age2)
+
+variable1 = 10
+print (variable1)
+
+h1='how are you?'
+print (h1)
+
+name = "Harsh" 'HARSH JHA'
+age = 20
+price = 100.50
+print (type(name))
+print (type(age))
+print (type(price))
+
+age = 20
+old = False
+a =None
+agee = none
+print (type(old))
+print (type(a))
