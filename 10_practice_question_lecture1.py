3ffa2bce55abcd4551a6ee3ddc90461bd58c6a2d
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-05-28 00:59:32 +0530

    feat:lecture 01 completed + 4 practice questions

---
+++ 10_practice_question_lecture1.py
@@ -0,0 +1,24 @@
+# #WAP to input 2 numbers and print there sum 
+
+# a=int(input("Enter first number: "))
+# b=int(input("Enter second number: "))
+# sum=a+b
+# print("The sum of", a, "and", b, "is", sum)
+
+# #WAP to input side of a square and print its area
+# a=int(input("Enter the side of the square: "))
+# area=a*a
+# print("Area of the square is :",area,"m\u00B2")
+
+# #WAP to input 2 floating point numbers and print thier average
+# a=float(input("Enter first number: "))
+# b=float(input("Enter second number: "))
+# avg=(a+b)/2
+# print("The average of", a, "and", b, "is", avg)
+
+#WAP to input 2 numbers a and b. print true if a is greater than or equal to b, if  not print false
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+true=a>=b
+print(true)
+print("is the number",a,"greater than equal to",b,":",true)
