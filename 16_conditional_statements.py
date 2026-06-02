da01123f9901bb42f7348e4efb34aebbe513af55
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-06-02 23:11:43 +0530

    Feat : if-elif-else completed

--- 16_conditional_statements.py
+++ 16_conditional_statements.py
@@ -1,15 +1,16 @@
-# age = 18 
-# if(age >= 18):
-#     print("You are eligible to vote.")
+#IF CONDITION 
+age = 18 
+if(age >= 18):
+    print("You are eligible to vote.")
 
-# age = 16
-# if(age >= 18):
-#     print("You are eligible to vote.")
+age = 16
+if(age >= 18):
+    print("You are eligible to vote.")
 
-# age = 25
-# if(age >= 18):
-#     print("can drive")
-#     print("can vote")
+age = 25
+if(age >= 18):
+    print("can drive")
+    print("can vote")
 
 age = 15
 if(True):
@@ -18,3 +19,37 @@ if(True):
 age = 24
 if(False):
     print("can drive can vote")             #This will execute nothing as the condition is false.
+
+
+#ELIF CONDITION [ELIF = ELSE IF]
+light ="green"
+if(light == "red"):
+    print("stop")
+elif(light == "yellow"):
+    print("get ready")
+elif(light == "green"):
+    print("go")
+
+    #Note : elif condition is only checked when the previous if or elif condition is false. 
+    # If the previous condition is true, then the elif condition will not be checked.
+num = 5
+if(num >2):
+    print("greater than 2")
+if(num >3):
+    print("greater than 3")
+elif(num>0):
+    print("zero")
+
+#ELSE CONDITON
+#it is onlhy used once at the end of if-elif ladder and it will execute when all the previous conditions are false.
+
+light = "blue"
+if(light == "red"):
+    print("stop")  
+elif(light == "yellow"):
+    print("get ready")
+elif(light == "green"):
+    print("go")
+else:
+    print("invalid light")
+
