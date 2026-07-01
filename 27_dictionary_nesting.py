ee73fdfe1f938ee9b6b90ce8c3423b7d7942584f
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-07-01 23:11:42 +0530

    nested dictionary

--- 27_dictionary_nesting.py
+++ 27_dictionary_nesting.py
@@ -1,1 +1,11 @@
-27_dictionary_nesting.py
+dict = {
+    "name" : "hats",
+    "relations" : {
+        "hats" : "pirates",
+        "italian hats" : "mafia",
+        "chinese hats" : "ninjas",
+    }
+} 
+print(dict)
+print(dict ["relations"])
+print(dict ["relations"] ["chinese hats"])
